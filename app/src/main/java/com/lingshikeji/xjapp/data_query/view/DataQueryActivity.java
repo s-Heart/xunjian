7d972cd426bc893333c42528f56f5bcbc1a0b661
tony <110618445@qq.com>
2017-03-27 00:02:32 +0800

    查询页面ui

--- app/src/main/java/com/lingshikeji/xjapp/data_query/view/DataQueryActivity.java
+++ app/src/main/java/com/lingshikeji/xjapp/data_query/view/DataQueryActivity.java
@@ -2,14 +2,21 @@ package com.lingshikeji.xjapp.data_query.view;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.lingshikeji.xjapp.R;
 import com.lingshikeji.xjapp.base.BaseActivity;
 import com.lingshikeji.xjapp.data_query.frame.IDataQueryPresenter;
 import com.lingshikeji.xjapp.data_query.frame.IDataQueryView;
 import com.lingshikeji.xjapp.data_query.presenter.DataQueryPresenterImpl;
+import com.lingshikeji.xjapp.tested_mgr.view.TestedDetailActivity;
+import com.lingshikeji.xjapp.tested_mgr.view.TestedMgrActivity;
 
 /**
  * <br/>Author: tony(shishaojie@koolearn.com)
@@ -22,6 +29,7 @@ import com.lingshikeji.xjapp.data_query.presenter.DataQueryPresenterImpl;
 public class DataQueryActivity extends BaseActivity implements IDataQueryView {
 
     private IDataQueryPresenter iDataQueryPresenter;
+    private TextView titleTextview;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,6 +41,34 @@ public class DataQueryActivity extends BaseActivity implements IDataQueryView {
 
     private void initView() {
         setContentView(R.layout.activity_data_query);
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        titleTextview = (TextView) toolbar.findViewById(R.id.toolbar_title);
+        titleTextview.setText("数据查询");
+        titleTextview.setVisibility(View.VISIBLE);
+
+
+        ImageView search = (ImageView) toolbar.findViewById(R.id.toolbar_right_search);
+        search.setVisibility(View.VISIBLE);
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DataQueryActivity.this, SearchActivity.class);
+                startActivity(intent);
+            }
+        });
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
     private void initData() {
---
+++ app/src/main/java/com/lingshikeji/xjapp/data_query/view/SearchActivity.java
@@ -0,0 +1,45 @@
+package com.lingshikeji.xjapp.data_query.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+
+import com.lingshikeji.xjapp.R;
+import com.lingshikeji.xjapp.base.BaseActivity;
+
+/**
+ * Created by tony on 2017/3/26.
+ */
+
+public class SearchActivity extends BaseActivity {
+    private TextView titleTextview;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initView();
+    }
+
+    private void initView() {
+        setContentView(R.layout.activity_search);
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        titleTextview = (TextView) toolbar.findViewById(R.id.toolbar_title);
+        titleTextview.setText("输入搜索条件");
+        titleTextview.setVisibility(View.VISIBLE);
+        toolbar.setNavigationIcon(R.drawable.ic_back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
